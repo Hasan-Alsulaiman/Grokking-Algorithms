32ed2f6a6bbb03bd89b7f1509b0ce3b204c497fc
Hasan <careerforma@gmail.com>
2021-05-29 19:15:51 +0300

    revert

--- Grokkings  Algorithms/src/DirectedGraph.java
+++ Grokkings  Algorithms/src/DirectedGraph.java
@@ -1,31 +1,31 @@
 import java.util.*;
 
 public class DirectedGraph {
+    HashMap<String, LinkedList<Map<String, Integer>>> graph;
 
-    public HashMap<String, Integer> graph;
-    public HashMap<String, String> parent;
     DirectedGraph() {
         graph = new HashMap<>();
-        parent = new HashMap<>();
     }
 
     public void add(String first, String second, Integer cost) {
         first = cleanStr(first);
         second = cleanStr(second);
-        graph.put(createKey(first,second), cost);
-        parent.put(second, first);
-    }
+        HashMap<String, Integer> map = new HashMap<>();
+        LinkedList<Map<String, Integer>> list = new LinkedList<>();
+        if(!graph.containsKey(first)){
+            map.put(second, cost);
+            list.add(map);
+            graph.put(first, list);
+        }else{
+            list = graph.get(first);
+            map.put(second, cost);
+            list.add(map);
+        }
 
-    private String createKey(String first, String second){
-        return first + "-" + second;
     }
 
     private String cleanStr(String input){
         return input.strip().toLowerCase(Locale.ROOT);
-    }
-
-    public Integer getCost(String first, String second){
-        return graph.get(createKey(first, second));
     }
 
     @Override
@@ -38,11 +38,6 @@ public class DirectedGraph {
         DirectedGraph g = new DirectedGraph();
         g.add("start", "a", 6);
         g.add("start", "b", 2);
-        g.add("b", "a", 3);
-        g.add("a", "fin", 1);
-        g.add("b", "fin", 5);
         System.out.println(g);
-        System.out.println(g.getCost("b","a"));
-        System.out.println(g.parent);
     }
 }
