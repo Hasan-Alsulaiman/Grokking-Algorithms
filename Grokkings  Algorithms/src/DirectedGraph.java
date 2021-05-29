390153f65119168f359c8096eae021e0e56a3333
Hasan <careerforma@gmail.com>
2021-05-29 19:01:50 +0300

    use one map in add

--- Grokkings  Algorithms/src/DirectedGraph.java
+++ Grokkings  Algorithms/src/DirectedGraph.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class DirectedGraph {
-    HashMap<String, LinkedList<Map<String, Integer>>> graph;
+    public HashMap<String, Integer> graph;
 
     DirectedGraph() {
         graph = new HashMap<>();
@@ -10,22 +10,19 @@ public class DirectedGraph {
     public void add(String first, String second, Integer cost) {
         first = cleanStr(first);
         second = cleanStr(second);
-        HashMap<String, Integer> map = new HashMap<>();
-        LinkedList<Map<String, Integer>> list = new LinkedList<>();
-        if(!graph.containsKey(first)){
-            map.put(second, cost);
-            list.add(map);
-            graph.put(first, list);
-        }else{
-            list = graph.get(first);
-            map.put(second, cost);
-            list.add(map);
-        }
+        graph.put(createKey(first,second), cost);
+    }
 
+    private String createKey(String first, String second){
+        return first + second;
     }
 
     private String cleanStr(String input){
         return input.strip().toLowerCase(Locale.ROOT);
+    }
+
+    public Integer getCost(String first, String second){
+        return graph.get(createKey(first, second));
     }
 
     @Override
@@ -38,6 +35,11 @@ public class DirectedGraph {
         DirectedGraph g = new DirectedGraph();
         g.add("start", "a", 6);
         g.add("start", "b", 2);
+        g.add("b", "a", 3);
+        g.add("a", "fin", 1);
+        g.add("b", "fin", 5);
         System.out.println(g);
+
+        System.out.println(g.getCost("b","a"));
     }
 }
