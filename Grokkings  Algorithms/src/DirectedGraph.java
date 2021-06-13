8f9f1059270316e5239514b447cd15ccfcb7950b
Hasan <careerforma@gmail.com>
2021-06-13 19:01:10 +0300

    start over

--- Grokkings  Algorithms/src/DirectedGraph.java
+++ Grokkings  Algorithms/src/DirectedGraph.java
@@ -2,42 +2,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class DirectedGraph {
-    HashMap<String, LinkedList<Map<String, Integer>>> graph;
 
-    DirectedGraph() {
-        graph = new HashMap<>();
-    }
-
-    public void add(String first, String second, Integer cost) {
-        first = cleanStr(first);
-        second = cleanStr(second);
-        HashMap<String, Integer> map = new HashMap<>();
-        LinkedList<Map<String, Integer>> list = new LinkedList<>();
-        if(!graph.containsKey(first)){
-            map.put(second, cost);
-            list.add(map);
-            graph.put(first, list);
-        }else{
-            list = graph.get(first);
-            map.put(second, cost);
-            list.add(map);
-        }
-
-    }
-
-    public List<Set<String>> neighboursOf(String node){
-        node = cleanStr(node);
-        return (graph.containsKey(node))? graph.get(node).stream()
-                .map(map -> map.keySet())
-                .collect(Collectors.toList()): null;
-    }
-
-    public Optional<Collection<Integer>> costOf(String first, String second) {
-        return graph.get(first).stream()
-                .filter(element -> element.containsKey(second))
-                .map(Map::values)
-                .findFirst();
-    }
 
     private String cleanStr(String input){
         return input.strip().toLowerCase(Locale.ROOT);
@@ -45,15 +10,15 @@ public class DirectedGraph {
 
     @Override
     public String toString() {
-        return "DirectedGraph" + graph;
+        return "Directed Graph " ;
     }
 
     public static void main(String[] args) {
         DirectedGraph g = new DirectedGraph();
-        g.add("start", "a", 6);
-        g.add("start", "b", 2);
-        g.add("b", "a", 1);
-        g.add("b", "fin", 2);
-        System.out.println(g.costOf("start", "a"));
+//        g.add("start", "a", 6);
+//        g.add("start", "b", 2);
+//        g.add("b", "a", 1);
+//        g.add("b", "fin", 2);
+//        System.out.println(g.costOf("start", "a"));
     }
 }
