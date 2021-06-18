8b2e99c9ee1fbdf0401b228b7221bc40a2ceae18
Hasan <careerforma@gmail.com>
2021-06-18 22:37:51 +0300

    implement get cheapest

--- Grokkings  Algorithms/src/Djikstra.java
+++ Grokkings  Algorithms/src/Djikstra.java
@@ -1,13 +1,31 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Djikstra {
     DirectedGraph graph;
     HashMap<String, Double> cost;
     HashMap<String, Double> parent;
+    ArrayList<String> processed;
+    String start;
 
-    public Djikstra(DirectedGraph graph) {
+    public Djikstra(DirectedGraph graph, String start) {
         this.graph = graph;
+        this.start = start;
         this.cost = new HashMap();
         this.parent = new HashMap();
+        this.processed = new ArrayList<>();
     }
+
+    private DirectedGraph.Edge getCheapest(List<DirectedGraph.Edge> nodes) {
+        DirectedGraph.Edge smallest = nodes.get(0);
+        for (DirectedGraph.Edge element : nodes) {
+            if(element.getWeight() < smallest.getWeight()){
+                smallest = element;
+            }
+        }
+        return smallest;
+    }
+
+
 }
