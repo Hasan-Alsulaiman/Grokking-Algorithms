5adab2cb1b531b5464a4a8870d81fbac4f87f58c
Hasan <careerforma@gmail.com>
2021-07-07 17:44:24 +0300

    implement calculateMinimumDistance

--- Grokkings  Algorithms/src/Djikstra/ShortestPath.java
+++ Grokkings  Algorithms/src/Djikstra/ShortestPath.java
@@ -1,6 +1,8 @@
 package Djikstra;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class ShortestPath {
@@ -20,5 +22,24 @@ public class ShortestPath {
             }
         }
         return smallestDistanceNode;
+    }
+
+    private void calculateMinimumDistance(Node evaluationNode, Integer edgeWeight, Node sourceNode) {
+
+    }
+
+    public WeightedGraph getShortestPath(Node source){
+        source.setDistance(0);
+        Set<Node> settled = new HashSet<>();
+        Set<Node> unSettled = new HashSet<>();
+        unSettled.add(source);
+        while (unSettled.size() != 0) {
+            Node current = getLowestDistanceNode(unSettled);
+            unSettled.remove(current);
+            for (Map.Entry<String, Integer> friends : current.getFriends().entrySet()) {
+
+            }
+        }
+        return null;
     }
 }
